488cda136e8ef7a89a2eb4ebc2b371ade7c6b336
Joanne <152332977+basest-selves@users.noreply.github.com>
2024-01-15 12:42:41 +0700

    Create book.py

---
+++ book.py
@@ -0,0 +1,9 @@
+# book.py
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    # Other methods
+    # ...
